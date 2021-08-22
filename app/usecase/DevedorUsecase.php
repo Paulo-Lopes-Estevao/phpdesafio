a9f02ff725b1bc9d9f31c9358faead5ee5a4abf4
Paulo Lopes Estevão <pl1745240@gmail.com>
2021-08-22 10:13:49 +0100

    edit: add usecase

---
+++ app/usecase/DevedorUsecase.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\app\usecase;
+
+use App\app\repository\interfaces\DevedorRepositoryInterface;
+use App\entities\Devedor;
+
+class DevedorUsecase{
+
+    private $devedorRepository;
+
+    public function __construct(DevedorRepositoryInterface $devedorRepositoryInterface)
+    {
+        $this->devedorRepository = $devedorRepositoryInterface;
+    }
+
+    public function AddDevedor(Devedor $devedor):void{
+        $this->devedorRepository->Create($devedor);
+    }
+
+    public function ShowAllDevedor()
+    {
+        $this->devedorRepository->Read();
+    }
+
+    public function UpdateDevedor(Devedor $devedor, int $id){
+        $this->devedorRepository->Update($devedor,$id);
+    }
+
+    public function DeleteDevedor(int $id)
+    {
+        $this->devedorRepository->Delete($id);
+    }
+
+}
