de7307f3f490deaa0ff935f569d1e374a68015a5
Paulo Lopes Estevão <pl1745240@gmail.com>
2021-08-22 11:13:17 +0100

    edit: tratement exception and beginTransaction

--- utils/database/connect/Connect.php
+++ utils/database/connect/Connect.php
@@ -1,64 +1,72 @@
-<?php 
+<?php
 
 namespace App\utils\database\connect;
 
-class Connect {
+class Connect
+{
 
 	private $conn;
 
-	public function __construct($drive,$dbname,$hostname,$username,$password)
+	public function __construct($drive, $dbname, $hostname, $username, $password)
 	{
 
 		$this->conn = new \PDO(
-			"$drive:dbname=".$dbname.";host=".$hostname, 
+			"$drive:dbname=" . $dbname . ";host=" . $hostname,
 			$username,
 			$password
 		);
-
 	}
 
 	private function setParams($statement, $parameters = array())
 	{
 
 		foreach ($parameters as $key => $value) {
-			
+
 			$this->bindParam($statement, $key, $value);
-
 		}
-
 	}
 
 	private function bindParam($statement, $key, $value)
 	{
 
 		$statement->bindParam($key, $value);
-
 	}
 
 	public function query($rawQuery, $params = array())
 	{
+		$this->conn->beginTransaction();
+		
+		try {
+			$stmt = $this->conn->prepare($rawQuery);
 
-		$stmt = $this->conn->prepare($rawQuery);
+			$this->setParams($stmt, $params);
 
-		$this->setParams($stmt, $params);
+			$stmt->execute();
 
-		$stmt->execute();
-
+			$this->conn->commit();
+		} catch (\PDOException $th) {
+			$this->conn->rollBack();
+			throw $th;
+		}
 	}
 
-	public function select($rawQuery, $params = array()):array
+	public function select($rawQuery, $params = array()): array
 	{
+		$this->conn->beginTransaction();
 
-		$stmt = $this->conn->prepare($rawQuery);
+		try {
+			$stmt = $this->conn->prepare($rawQuery);
 
-		$this->setParams($stmt, $params);
+			$this->setParams($stmt, $params);
 
-		$stmt->execute();
+			$stmt->execute();
 
-		return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+			return $stmt->fetchAll(\PDO::FETCH_ASSOC);
 
+			$this->conn->commit();
+		} catch (\PDOException $th) {
+			$this->conn->rollBack();
+			throw $th;
+		}
 	}
-
 }
-
- ?>
