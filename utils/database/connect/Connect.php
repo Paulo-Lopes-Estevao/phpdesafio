cb7cd82903f85e78fe0fddbb17cedbfc5521f09e
Paulo Lopes Estevão <pl1745240@gmail.com>
2021-08-22 10:15:58 +0100

    edit: add connection db

---
+++ utils/database/connect/Connect.php
@@ -0,0 +1,64 @@
+<?php 
+
+namespace App\utils\database\connect;
+
+class Connect {
+
+	private $conn;
+
+	public function __construct($drive,$dbname,$hostname,$username,$password)
+	{
+
+		$this->conn = new \PDO(
+			"$drive:dbname=".$dbname.";host=".$hostname, 
+			$username,
+			$password
+		);
+
+	}
+
+	private function setParams($statement, $parameters = array())
+	{
+
+		foreach ($parameters as $key => $value) {
+			
+			$this->bindParam($statement, $key, $value);
+
+		}
+
+	}
+
+	private function bindParam($statement, $key, $value)
+	{
+
+		$statement->bindParam($key, $value);
+
+	}
+
+	public function query($rawQuery, $params = array())
+	{
+
+		$stmt = $this->conn->prepare($rawQuery);
+
+		$this->setParams($stmt, $params);
+
+		$stmt->execute();
+
+	}
+
+	public function select($rawQuery, $params = array()):array
+	{
+
+		$stmt = $this->conn->prepare($rawQuery);
+
+		$this->setParams($stmt, $params);
+
+		$stmt->execute();
+
+		return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+
+	}
+
+}
+
+ ?>
