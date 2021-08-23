cebf98abf12a96c7cba1d6b77f6ec64d1f0411ad
Paulo Lopes Estevão <pl1745240@gmail.com>
2021-08-23 02:43:58 +0100

    edit: alter repository in CRUD process

--- app/repository/DevedorRepository.php
+++ app/repository/DevedorRepository.php
@@ -4,32 +4,58 @@ namespace App\app\repository;
 
 use App\app\repository\interfaces\DevedorRepositoryInterface;
 use App\entities\Devedor;
+use App\utils\database\mysql\MysqlDataBase;
 
-class DevedorRepository implements DevedorRepositoryInterface{
+class DevedorRepository implements DevedorRepositoryInterface
+{
 
-    private $devedorRepository;
+    private $db;
 
     public function __construct()
     {
-        
+        $this->db = new MysqlDataBase();
     }
 
-    public function Create(Devedor $devedor):void{
+    public function Create(Devedor $devedor): array
+    {
+                $result = $this->db->select("INSERT INTO tb_devedor(nome,cpf,datanascimento,endereco,descricaotitulo,valor,datavencimento) 
+                VALUES(:nome,:cpf,:datanascimento,:endereco,:descricaotitulo,:valor,:datavencimento)", array(
+                    ":nome" => $devedor->getNome(),
+                    ":cpf" => $devedor->getCpf(),
+                    ":datanascimento" => $devedor->getDataNascimento(),
+                    ":endereco"=>$devedor->getEndereco(),
+                    ":descricaotitulo" => $devedor->getDescricaoTitulo(),
+                    ":valor" => $devedor->getValor(),
+                    ":datavencimento" => $devedor->getDataVencimento()
+                ));
         
+                return $result;
+
     }
 
     public function Read()
     {
-        
+        $result = $this->db->select("SELECT * from tb_devedor");
+        return $result;
     }
 
-    public function Update(Devedor $devedor, int $id){
-        
+    public function Update(Devedor $devedor, int $id)
+    {
+        $result = $this->db->query("UPDATE tb_usuarios SET nome = :nome, cpf = :cpf, datanascimento = :datanascimento, endereco = :endereco, descricaotitulo = :descricaotitulo, valor = :valor, datavencimento = :datavencimento WHERE id = :ID",array(
+            ":nome" => $devedor->getNome(),
+            ":cpf" => $devedor->getCpf(),
+            ":datanascimento" => $devedor->getDataNascimento(),
+            ":endereco"=>$devedor->getEndereco(),
+            ":descricaotitulo" => $devedor->getDescricaoTitulo(),
+            ":valor" => $devedor->getValor(),
+            ":datavencimento" => $devedor->getDataVencimento(),
+            ':ID'=> $id
+        ));
+        return $result;
     }
 
     public function Delete(int $id)
     {
         $this->devedorRepository->Delete($id);
     }
-
 }
