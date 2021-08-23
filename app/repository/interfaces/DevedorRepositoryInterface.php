9bdb7f8bbcceeb6c2a7db769557406c5422bf0be
Paulo Lopes Estevão <pl1745240@gmail.com>
2021-08-23 02:45:02 +0100

    repository interface defined

--- app/repository/interfaces/DevedorRepositoryInterface.php
+++ app/repository/interfaces/DevedorRepositoryInterface.php
@@ -5,7 +5,7 @@ namespace App\app\repository\interfaces;
 use App\entities\Devedor;
 
 interface DevedorRepositoryInterface{
-    public function Create(Devedor $devedor):void;
+    public function Create(Devedor $devedor):array;
     public function Read();
     public function Update(Devedor $devedor,int $id);
     public function Delete(int $id);
