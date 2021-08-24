4f14315835f13ecb0bf89d9316add2c808129192
Paulo Lopes Estevão <pl1745240@gmail.com>
2021-08-24 03:05:12 +0100

    config controller base index view

---
+++ controllers/BaseController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\controllers;
+
+class BaseController{
+
+    public function views($file, $data = array()){
+        $directory = '.'. DIRECTORY_SEPARATOR . 'desafiophp' . DIRECTORY_SEPARATOR . 'views'. DIRECTORY_SEPARATOR . $file . '.php';
+        $file = $directory;
+
+        if(file_exists($file)){
+            require $file;
+        }
+    }
+
+}
